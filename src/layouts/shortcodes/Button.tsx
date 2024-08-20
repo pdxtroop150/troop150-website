6ad004de07f9dd2618310a3ba867aa2af3b27bec
readerkid <74160180+readerkid@users.noreply.github.com>
2024-08-20 03:06:49 +0000

    make join button open in new tab and all buttons open in same tab

--- src/layouts/shortcodes/Button.tsx
+++ src/layouts/shortcodes/Button.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import type Tab from "./Tab";
 
 const Button = ({
   label,
   link,
   style,
   rel,
+  open_in_new_tab,
 }: {
   label: string;
   link: string;
   style?: string;
   rel?: string;
+  open_in_new_tab?: boolean;
 }) => {
   return (
     <a
       href={link}
-      target="_blank"
+      target={open_in_new_tab ? "_blank" : undefined}
       rel={`noopener noreferrer ${
         rel ? (rel === "follow" ? "" : rel) : "nofollow"
       }`}
